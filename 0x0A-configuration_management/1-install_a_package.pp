b979ef55c11032e72fa3cf6bfb34147234767d5b
Patrick A. Noblet <appiahpatricknoblet@gmail.com>
2024-04-20 00:10:57 +0000

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # Puppet manifest to install flask from pip3
 
 package { 'flask':
-ensure 	 => '2.1.0', # Ensure Flask version 2.1.0 is installed
-provider => 'pip3',  # Specify the package provider as pip3
-}
+ensure 	 => '2.1.0',
+provider => 'pip3',
