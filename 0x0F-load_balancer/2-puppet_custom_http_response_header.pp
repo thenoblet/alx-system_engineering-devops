d49b0877131450848fb3a2a9f11d705f67fe4200
Patrick A. Noblet <appiahpatricknoblet@gmail.com>
2024-04-30 03:07:34 +0000

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,15 +1,15 @@
-# Update package information
+# Update package 
 exec { 'apt-update':
   command => '/usr/bin/apt-get -y update',
   path    => ['/usr/bin', '/bin'],
 }
 
-# Install Nginx package
+# Install the Nginx package 
 package { 'nginx':
   ensure => installed,
 }
 
-# Configure Nginx to add the custom HTTP header
+# Configure Nginx to add the custom HTTP header 
 file_line { 'add custom header':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
@@ -17,7 +17,9 @@ file_line { 'add custom header':
   after  => 'server_name _;',
 }
 
-# Ensure Nginx is running
+# Ensure Nginx service is running and enabled
 service { 'nginx':
   ensure => running,
+  enable  => true,
+  require => Package['nginx'],
 }
