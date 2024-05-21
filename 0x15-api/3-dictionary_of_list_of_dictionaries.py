42a4e71689476f3193418e01efc2ab6cfd6096d2
noblet <appiahpatricknoblet@gmail.com>
2024-05-21 01:18:49 +0100

    added task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,28 +1,43 @@
-
-#!/usr/bin/python3
-""" Export data in the JSON format. """
-import json
 import requests
-import sys
+import json
 
 
 if __name__ == "__main__":
-    response = requests.get("https://jsonplaceholder.typicode.com/users")
-    dicti = json.loads(response.text)
-    ans = {}
-    for user in dicti:
-        username = user.get('username')
-        userId = str(user.get('id'))
-        response = requests.get("https://jsonplaceholder.typicode.com/todos/" +
-                                "?userId=" + userId)
-        todos = json.loads(response.text)
-        tasks = [task for task in todos]
-        ans[userId] = []
-        for task in tasks:
-            task_dict = {"task": task.get('title'),
-                         'completed': task.get('completed'),
-                         'username': username}
-            ans.get(userId).append(task_dict)
-    with open("todo_all_employees.json", "w") as f:
-        json.dump(ans, f)
+    # Define the URLs
+    url_users = 'https://jsonplaceholder.typicode.com/users'
+    url_todos = 'https://jsonplaceholder.typicode.com/todos'
 
+    # Fetch data from the URLs
+    response_users = requests.get(url_users)
+    response_todos = requests.get(url_todos)
+
+    # Convert response data to JSON
+    users = response_users.json()
+    todos = response_todos.json()
+
+    # Create a dictionary to store the data
+    data = {}
+
+    # Process users and todos
+    for user in users:
+        user_id = str(user.get('id'))
+        username = user.get('username')
+
+        # Filter todos for the current user
+        user_todos = [
+                todo for todo in todos if todo.get('userId') == user.get('id')
+        ]
+
+        # Structure the tasks
+        tasks = [{
+            'username': username,
+            'task': todo.get('title'),
+            'completed': todo.get('completed')
+        } for todo in user_todos]
+
+        # Add the tasks to the dictionary under the user_id key
+        data[user_id] = tasks
+
+        # Write the data to a JSON file
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(data, json_file, indent=4)
