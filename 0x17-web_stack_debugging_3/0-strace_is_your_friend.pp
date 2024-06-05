e1453842aeac8b69ca4f20dc4c7e76d24600c88f
Patrick A. Noblet <appiahpatricknoblet@gmail.com>
2024-06-05 19:07:41 +0000

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,13 @@
 # Define the file path as a variable
-$file_path = '/var/www/html/wp-settings.php'
 
-# Ensure the file exists before making changes
+$file_path = '/var/www/html/wp-settings.php'
 file { $file_path:
   ensure => file,
 }
 
-# Use exec to replace 'phpp' with 'php' in the file
 exec { 'replace_phpp_with_php':
-  command     => "/usr/bin/sed -i 's/phpp/php/g' ${file_path}",
-  path        => ['/bin', '/usr/bin'],
+  command     => "sed -i 's/phpp/php/g' ${file_path}",
+  path        => ['/bin', '/usr/bin', '/usr/sbin/'],
   refreshonly => true,
-  subscribe   => File[$file_path],
+  require     => File[$file_path],
 }
