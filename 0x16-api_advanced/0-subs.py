66fc6cde63ebd788c3e961fa2bd38a0597e7994f
noblet <appiahpatricknoblet@gmail.com>
2024-06-05 03:37:31 +0100

    Added a a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
-"""This script queries the Reddit API and returns the number of total
-subscribers for a given subreddit."""
+
+"""
+This module queries the Reddit API to retrieve the number of subscribers
+for a given subreddit. It contains a single function, number_of_subscribers,
+which takes the name of a subreddit as an argument and returns the number
+of subscribers to that subreddit.
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Retrieves the number of subscribers for a given subreddit.
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
 
     Args:
-        subreddit (str): The name of the subreddit.
+        subreddit (str): The name of the subreddit to query.
 
     Returns:
-        int: The number of subscribers for the subreddit. Returns 0 if the
-        request fails or the subreddit does not exist.
+        int: The number of subscribers to the subreddit. If the subreddit does
+        not exist or there is an error, the function returns 0.
     """
-    response = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        allow_redirects=False,
-        headers={"user-agent": "nabuntu_bot-01"},
-        timeout=60,
-    )
 
-    return (
-        response.json()["data"]["subscribers"]
-        if response.status_code == 200
-        else 0
-    )
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Custom"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data["data"]["subscribers"]
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
